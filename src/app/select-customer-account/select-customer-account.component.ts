4b05d7bc3cd465ef03268d28b368aa255629ae92
Te Vallee <astronomemusic@gmail.com>
2017-11-20 17:19:20 -1000

    Select Customer Account fetches all accounts

--- src/app/select-customer-account/select-customer-account.component.ts
+++ src/app/select-customer-account/select-customer-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { SelectCustomerAccountService } from './select-customer-account.service';
 
@@ -11,9 +12,31 @@ import { SelectCustomerAccountService } from './select-customer-account.service'
 })
 export class SelectCustomerAccountComponent implements OnInit {
 
-  constructor(service: SelectCustomerAccountService) { }
+  form : FormGroup;
+  accounts : any;
+
+  constructor(private service: SelectCustomerAccountService, private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      account: ['', Validators.compose([Validators.required])]
+    });
+    this.getAccounts();
+  }
+
+  getAccounts() {
+    this.service.getAccounts()
+      .subscribe(
+        accounts => {
+          this.accounts = accounts;
+          this.accounts = this.accounts.sort((a,b) => a.user.email - b.user.emial);
+        },
+        err => this.service.handleError(err)
+      );
+  }
+
+  selectAccount(account: any) {
+
   }
 
 }
--- src/app/select-customer-account/select-customer-account.service.ts
+++ src/app/select-customer-account/select-customer-account.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { environment } from '../../environments/environment';
+
 @Injectable()
 export class SelectCustomerAccountService {
 
-  constructor() { }
+  private headers : HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    let id_token = localStorage.getItem('id_token');
+    this.headers = new HttpHeaders().set('Authorization', `bearer ${id_token}`);
+  }
+
+  getAccounts() {
+    return this.http.get(`${environment.domain}/api/accounts`, { headers: this.headers });
+  }
+
+  handleError(err) {
+    console.error(err);
+  }
 
 }
