475f73b3bedefaf3f0d51106035b4bcdc6894d70
Te Vallee <astronomemusic@gmail.com>
2017-11-29 13:16:21 -1000

    has_no_shipments added to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   domain: 'https://domain.airshipcms.io',
   stripe_publish_key: 'xxxx',
-  skip_single_payment_shipping: true,
-  skip_subscription_shipping: true,
+  skip_single_payment_shipping: false,
+  skip_subscription_shipping: false,
+  has_no_shipments: false,
   default_address: {}
 };
