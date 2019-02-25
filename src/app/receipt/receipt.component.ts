7f2833659e4ca2f12618a7da81ac35f16581dc6c
Te Vallee <astronomemusic@gmail.com>
2019-02-25 13:58:45 -0800

    Refreshing on receipt view redirects to root

--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { SharedService } from '../shared.service';
 import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-receipt',
@@ -14,66 +15,70 @@ export class ReceiptComponent implements OnInit {
   receipt : any;
   failedOrders : Array<any> = [];
 
-  constructor(public sharedService: SharedService) {}
+  constructor(public sharedService: SharedService, public router: Router) {}
 
   ngOnInit() {
-    if(this.sharedService.checkoutResponse.products) {
-      environment.shipping_types.forEach((type) => {
-        if(this.sharedService.checkoutResponse.single_payment.shipping_type.toLowerCase() === type.toLowerCase()) {
-          this.sharedService.checkoutResponse.single_payment.shipping_type = type;
-        }
-      });
-      this.receipt = this.sharedService.checkoutResponse;
+    if(this.sharedService.checkoutResponse) {
+      if(this.sharedService.checkoutResponse.products) {
+        environment.shipping_types.forEach((type) => {
+          if(this.sharedService.checkoutResponse.single_payment.shipping_type.toLowerCase() === type.toLowerCase()) {
+            this.sharedService.checkoutResponse.single_payment.shipping_type = type;
+          }
+        });
+        this.receipt = this.sharedService.checkoutResponse;
+      } else {
+        let subscriptions = { items: [] };
+        let plans = [];
+        let customers = [];
+        let products = {};
+        let single_payment = {};
+        let account = {};
+        let customer = {};
+        let shipping_address = {};
+        let subscription_addresses = [];
+        let creditCard = JSON.parse(localStorage.getItem('card'));
+        this.sharedService.checkoutResponse.forEach((item) => {
+          if(item.account) {
+            if(item.products.items.length > 0) {
+              products = item.products;
+              single_payment = item.single_payment;
+              environment.shipping_types.forEach((type) => {
+                if(single_payment['shipping_type'].toLowerCase() === type.toLowerCase()) {
+                  single_payment['shipping_type'] = type;
+                }
+              });
+              shipping_address = item.shipping_address;
+              account = item.account;
+              customer = item.customer;
+            }
+            if(item.subscriptions.length > 0) {
+              subscription_addresses.push(item.shipping_address);
+              item.subscriptions[0].misc_data = item.plans[0].misc_data;
+              subscriptions.items.push(item.subscriptions[0]);
+              customers.push(item.customer);
+              plans.push(item.plans[0]);
+            }
+          } else {
+            this.failedOrders.push(item);
+          }
+        });
+        this.receipt = Object.assign({}, {
+          subscription_addresses,
+          account,
+          shipping_address,
+          customer,
+          customers,
+          single_payment,
+          products,
+          subscriptions,
+          plans,
+          creditCard
+        });
+      }
+      this.sharedService.clearLocalStorage();
     } else {
-      let subscriptions = { items: [] };
-      let plans = [];
-      let customers = [];
-      let products = {};
-      let single_payment = {};
-      let account = {};
-      let customer = {};
-      let shipping_address = {};
-      let subscription_addresses = [];
-      let creditCard = JSON.parse(localStorage.getItem('card'));
-      this.sharedService.checkoutResponse.forEach((item) => {
-        if(item.account) {
-          if(item.products.items.length > 0) {
-            products = item.products;
-            single_payment = item.single_payment;
-            environment.shipping_types.forEach((type) => {
-              if(single_payment['shipping_type'].toLowerCase() === type.toLowerCase()) {
-                single_payment['shipping_type'] = type;
-              }
-            });
-            shipping_address = item.shipping_address;
-            account = item.account;
-            customer = item.customer;
-          }
-          if(item.subscriptions.length > 0) {
-            subscription_addresses.push(item.shipping_address);
-            item.subscriptions[0].misc_data = item.plans[0].misc_data;
-            subscriptions.items.push(item.subscriptions[0]);
-            customers.push(item.customer);
-            plans.push(item.plans[0]);
-          }
-        } else {
-          this.failedOrders.push(item);
-        }
-      });
-      this.receipt = Object.assign({}, {
-        subscription_addresses,
-        account,
-        shipping_address,
-        customer,
-        customers,
-        single_payment,
-        products,
-        subscriptions,
-        plans,
-        creditCard
-      });
+      this.router.navigate(['/checkout']);
     }
-    this.sharedService.clearLocalStorage();
   }
 
 }
